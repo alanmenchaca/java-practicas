423655c7fce0f12fa8134a29be44f62a6062661d
Fco. Alan Menchaca M <alanmenchaca22@outlook.com>
2020-09-20 12:53:36 -0500

    Constantes (math.PI & math.E) con la librería Math

---
+++ java-intermedio/clase-math/ejemplos/Constantes.java
@@ -0,0 +1,10 @@
+package ejemplos;
+
+public class Constantes {
+
+    public static void main(String[] args) {
+
+        System.out.println("El número e: " + Math.E);
+        System.out.println("El número pi: " + Math.PI);
+    }
+}
